fc86bade19436622e1e245cb2d4d8ef7647bd994
jessiropa <kristinjessi9700@gmail.com>
2023-03-05 20:45:45 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World!!!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum atque nihil, doloremque nostrum dolores a aliquid ut accusamus, nobis dicta libero vel! Tempore explicabo ratione dolore atque, blanditiis minus odio.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+        <li>Tiga</li>
+    </ul>
 </body>
 </html>
